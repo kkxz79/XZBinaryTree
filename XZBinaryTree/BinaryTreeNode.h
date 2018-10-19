0e5d5d87901b7699cb23024ff20194bced22209e
kkxz79 <kkxz79@126.com>
2018-10-19 16:43:19 +0800

    first commit

---
+++ XZBinaryTree/BinaryTreeNode.h
@@ -0,0 +1,17 @@
+//
+//  BinaryTreeNode.h
+//  XZBinaryTree
+//
+//  Created by kkxz on 2018/10/17.
+//  Copyright © 2018年 kkxz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BinaryTreeNode : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XZBinaryTree/BinaryTreeNode.m
@@ -0,0 +1,13 @@
+//
+//  BinaryTreeNode.m
+//  XZBinaryTree
+//
+//  Created by kkxz on 2018/10/17.
+//  Copyright © 2018年 kkxz. All rights reserved.
+//
+
+#import "BinaryTreeNode.h"
+
+@implementation BinaryTreeNode
+
+@end
